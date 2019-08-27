e3f5701a8dc5179248d4781eb1ffdd480b488e03
dustine32 <dustine32@gmail.com>
2019-08-26 17:19:46 -0700

    0.0.5 release. Build format data models and BuildConfig.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pthr_db_caller",
-    version='0.0.3',
+    version='0.0.5',
     packages=find_packages(),
     author="dustine32",
     author_email="debert@usc.edu",
@@ -10,6 +10,8 @@ setup(
     long_description=open("README.md").read(),
     url="https://github.com/pantherdb/fullgo_paint_update",
     install_requires=[
-        "psycopg2==2.7.4"
+        "psycopg2==2.7.4",
+        "biopython==1.73",
+        "networkx==2.3"
     ]
 )
