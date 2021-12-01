6b7a3909c110a0f5f0f74306f76f80dd659db3c2
dustine32 <dustine32@gmail.com>
2021-11-30 18:29:10 -0800

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pthr_db_caller",
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author="dustine32",
     author_email="debert@usc.edu",
